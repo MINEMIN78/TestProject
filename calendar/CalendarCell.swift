7bd1c9d6d99b0c2f8ea5e2ad8e4a0b48071d50f4
YUMAKOMORI <YUMAKOMORI@Yumas-MacBook-Pro.local>
2016-06-23 18:12:09 +0900

    first commit

---
+++ calendar/CalendarCell.swift
@@ -0,0 +1,29 @@
+//
+//  CalendarCell.swift
+//  calendar
+//
+//  Created by YUMAKOMORI on 2016/06/12.
+//  Copyright © 2016年 YUMAKOMORI. All rights reserved.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    var textLabel: UILabel!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        
+        // UILabelを生成
+        textLabel = UILabel(frame: CGRectMake(0, 0, self.frame.width, self.frame.height))
+        textLabel.font = UIFont(name: "HiraKakuProN-W3", size: 12)
+        textLabel.textAlignment = NSTextAlignment.Center
+        // Cellに追加
+        self.addSubview(textLabel!)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+}
